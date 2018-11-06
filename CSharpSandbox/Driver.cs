f8f5e53bdef5946712263003448b474743ea36fd
Brennan <brennahan@gmail.com>
2018-11-06 09:47:44 -0600

    Latest code, mostly just playing around with lambdas, Func, Action.

--- CSharpSandbox/Driver.cs
+++ CSharpSandbox/Driver.cs
@@ -10,21 +10,22 @@ namespace CSharpSandbox
 	{
 		static void Main(string[] args)
 		{
-			DPC303.Hard();
-			//DPC364.Easy();
-			//DPC364.Intermediate();
-			//DPC365.Easy();
-			//DPC365.Intermediate();
-			//Dijkstra.Calculate();
+            //DPC303.Hard();
+            //DPC364.Easy();
+            //DPC364.Intermediate();
+            //DPC365.Easy();
+            //DPC365.Intermediate();
 
-
+            //Lambdas.LambdaPractice((i, j, k) => i*j*k);
+            Lambdas.LambdaPractice2(i => i.myVal++);
+            
+            //Dijkstra.Calculate();
 
 			//foreach (var rel in Dijkstra.GenerateNodeTree(10))
 			//{
 			//	Console.WriteLine(rel.start + " -> " + rel.end + ": " + rel.distance + " units");
 			//}
 			//Console.Read();
-
 		}
 	}
 }
---
+++ CSharpSandbox/Lambdas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSandbox
+{
+    class Lambdas
+    {
+        public static void LambdaPractice(Func<int, int, int, int> predicate)
+        {
+            int input1 = 2;
+            int input2 = 5;
+            int input3 = 7;
+            var result = predicate(input1, input2, input3);
+
+            Console.WriteLine("Input(s): " + input1 + ", " + input2 + ", " + input3);
+            Console.WriteLine("Result: " + result);
+            Console.Read();
+        }
+
+        public static void LambdaPractice2(Action<customObject> action)
+        {
+            customObject temp = new customObject();
+            temp.myVal = 10;
+
+            Console.WriteLine("Input(s): " + temp.myVal);
+            action.Invoke(temp);
+            Console.WriteLine("Result: " + temp.myVal);
+            Console.Read();
+        }
+    }
+
+    class customObject
+    {
+        public int myVal { get; set; }
+    }
+}
